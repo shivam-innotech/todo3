15b31a92d9a28e0558e1e32bb802ad1c5165acee
shivam-innotech <Shivam@innotechteam.in>
2022-09-15 18:44:22 +0530

    'comp.'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import ComponentOne from './components/ComponentOne'
 
--- src/components/ComponentOne.js
+++ src/components/ComponentOne.js
@@ -3,16 +3,48 @@ import React, { useState } from 'react'
 const ComponentsOne = () => {
     const [inputData, setInputData] = useState('');
     const [items, setItems] = useState([]);
+    const [toggleSubmit, setToggleSubmit] = useState(true);
+    const [isEditItem, setIsEditItem] = useState(null);
 
     const addItem = () => {
         if (!inputData) {
+            alert('fill it');
+        }
+        else if (inputData && !toggleSubmit) {
+            setItems(
+                items.map((elem) => {
+                    if (elem.id === isEditItem) {
+                        return { ...elem, name: inputData }
+                    }
+                    return elem;
+                })
+            )
+            setToggleSubmit(true);
 
-        } else {
-            setItems([...items, inputData]);
+            setInputData('');
+
+            setIsEditItem(null);
+        }
+        else {
+            const allInputData = { id: new Date().getTime().toString(), name: inputData }
+            setItems([...items, allInputData]);
             setInputData('');
         }
-
     }
+
+    const editItem = (id) => {
+        let newEditItem = items.find((elem) => {
+            return elem.id === id
+        });
+        console.log(newEditItem);
+
+        setToggleSubmit(false);
+
+        setInputData(newEditItem.name);
+
+        setIsEditItem(id);
+    }
+
     return (
         <>
             <div className="main-div">
@@ -21,15 +53,20 @@ const ComponentsOne = () => {
                         value={inputData}
                         onChange={(e) => setInputData(e.target.value)}
                     />
-                    <button type="submit" onClick={addItem}>Submit</button>
+                    {
+                        toggleSubmit ? <button type="submit" onClick={addItem}>Submit</button> :
+                            <button onClick={addItem}>Edit</button>
+                    }
+
                 </div>
 
                 <div className="showItems">
                     {
-                        items.map((elem, ind) => {
+                        items.map((elem) => {
                             return (
-                                <div className="eachItem" key={ind}>
-                                    <h1>{elem}</h1>
+                                <div className="eachItem" key={elem.id}>
+                                    <h1>{elem.name}</h1>
+                                    <button onClick={() => editItem(elem.id)}>Edit</button>
                                 </div>
                             )
                         })
